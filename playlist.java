c0620db42b97dc3a6290e9509861b06ff26888f6
sanjupreetham <sanjupreetham22@gmail.com>
2025-12-17 10:12:06 +0530

    Add files via upload

---
+++ playlist.java
@@ -0,0 +1,9 @@
+public class playlist {
+    public static void main(String[] args) {
+        String[] songs = {"Song A", "Song B", "Song C", "Song D"};
+        System.out.println("Playlist:");
+        for (String song : songs) {
+            System.out.println(song);
+        }
+    }
+}
